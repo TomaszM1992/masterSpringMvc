4c82b55a4fa3cfaa68e47d2f80b59739aef392c3
TomaszM1992 <tomek.m.osw@gmail.com>
2017-03-24 12:01:20 +0100

    'StaticManPage'

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	@ResponseBody
 	public String hello(){
-		return "Hallo, Welt";
+		return "resultPage";
 	}
 }
