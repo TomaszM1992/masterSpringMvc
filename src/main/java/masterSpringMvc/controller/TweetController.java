3ff7397e3269d3edf28f6847b8890b024e94e9ed
TomaszM1992 <tomek.m.osw@gmail.com>
2017-03-27 15:44:29 +0200

    Materialize and CSS jQuerry implementation

--- src/main/java/masterSpringMvc/controller/TweetController.java
+++ src/main/java/masterSpringMvc/controller/TweetController.java
@@ -1,15 +1,14 @@
 package masterSpringMvc.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.twitter.api.*;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
 @Controller
 public class TweetController {
@@ -20,8 +19,9 @@ public class TweetController {
 	@RequestMapping("/")
 	public String hello(@RequestParam(defaultValue = "TajnikiSpringMVC4") String search, Model model){
 		SearchResults searchResults = twitter.searchOperations().search(search);
-		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+		List<Tweet> tweets = searchResults.getTweets();
 		model.addAttribute("tweets",tweets);
+		model.addAttribute("search", search);
 		return "resultPage";
 	}
 	
